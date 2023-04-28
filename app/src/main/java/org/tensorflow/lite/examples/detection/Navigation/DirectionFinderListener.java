ae141bf1f3976825abfbefff0ce4ee2f0d882137
rushikeshty <117820507+rushikeshty@users.noreply.github.com>
2023-04-28 09:08:48 +0530

    Delete app/src/main/java/org/tensorflow/lite/examples/detection/Navigation directory

--- app/src/main/java/org/tensorflow/lite/examples/detection/Navigation/DirectionFinderListener.java
+++
@@ -1,9 +0,0 @@
-package org.tensorflow.lite.examples.detection.Navigation;
-
-
-import java.util.List;
-
- public interface DirectionFinderListener {
-    void onDirectionFinderStart();
-    void onDirectionFinderSuccess(List<Route> route);
-}
